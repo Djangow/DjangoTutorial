0917d8dc03ab5ca93b0d6cb500938e14e9466133
lhyngrha <gilsongabriel@MBP-de-Gilson.home>
2016-08-14 11:09:50 -0300

    Tests 75%

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 
 from .models import Question, Choice
@@ -48,7 +49,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
