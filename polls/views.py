2a45759c3c19a50a03b619167e4e2599172e5196
lhyngrha <gilsongabriel@MBP-de-Gilson.home>
2016-08-13 23:09:26 -0300

    4 Finalizado

--- polls/views.py
+++ polls/views.py
@@ -44,7 +44,22 @@ def results(request, question_id):
 
 
 class IndexView(generic.ListView):
-    
+    template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
