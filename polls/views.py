550c666193c5573e10487623acf5269dffbb0a7c
lhyngrha <gilsongabriel@MBP-de-Gilson.home>
2016-08-13 21:05:20 -0300

    Results - Problema

--- polls/views.py
+++ polls/views.py
@@ -54,7 +54,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        #return HttpResponse("Arho")
+        #print reverse('polls:results', args=(question.id,))
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
